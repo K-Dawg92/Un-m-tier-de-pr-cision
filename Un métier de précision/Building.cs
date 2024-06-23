e1e4394d7cada05b48ab9d52420fd878c41142f1
K-Dawg92 <kevin.osei.yaw@gmail.com>
2024-06-23 23:36:02 +0200

    Ajoutez des fichiers projet.

---
+++ Un métier de précision/Building.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Un_métier_de_précision
+{
+    internal class Building
+    {
+        public int floorLevel {  get; set; }
+        public int heightOfBuilding { get; set; }
+       
+        // constructor initialising both floor level and height of building
+        public Building(int floorLevel, int heightBuilding)
+        {
+            this.floorLevel = floorLevel;
+            this.heightOfBuilding = heightBuilding;
+        }
+        // constructor initialising only the floor level by calculating the height
+        public Building(int floorLevel) 
+        {
+            this.floorLevel = floorLevel;    
+            this.heightOfBuilding = floorLevel*3;
+        }
+        //  renvoie la taille maximum que peut faire un étage du bâtiment
+        public int GetFloorMaxSize()
+        {
+            return heightOfBuilding / floorLevel;
+        }
+        // renvoie la taille du bâtiment
+        public int GetSize()
+        {
+            return heightOfBuilding;
+        }
+        // renvoie le nombre d'étages du bâtiment
+        public int GetFloorCount()
+        {
+            return floorLevel*3;
+        }
+    }
+}
---
+++ Un métier de précision/Program.cs
@@ -0,0 +1,29 @@
+﻿using Un_métier_de_précision;
+
+Console.WriteLine("Please enter how many floors are required in your building");
+int floorLevels = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the height of your building");
+int heightOfBuilding = int.Parse(Console.ReadLine());   
+
+Building building1 = new Building(floorLevels,heightOfBuilding);
+
+
+Console.WriteLine($"Building 1 size = {building1.GetSize()} meters");
+
+Console.WriteLine("Please enter the number of floors required");
+int floorlevelbuilding2 = int.Parse(Console.ReadLine());
+
+Building building2 = new Building(floorlevelbuilding2);
+Console.WriteLine($"Building 2 size = {building2.GetFloorCount()} meters");
+
+Console.WriteLine("Please enter height of building");
+int heightMaxSize = int.Parse(Console.ReadLine());
+Console.WriteLine("Now enter the floor level");
+int floorMaxSize = int.Parse(Console.ReadLine());
+
+Building building3 = new Building(heightMaxSize, floorMaxSize);
+Console.WriteLine($"The maximum size of A floor level is {building3.GetFloorMaxSize()}  meters");
+
+
+
+
